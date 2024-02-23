e37a16f57b3237844c8543e21db6eb6b71094d51
Rhonalf Martinez <rhonalf.martinez@koombea.com>
2024-02-23 17:25:28 -0500

    Session 1

---
+++ assets/scripts/index.js
@@ -0,0 +1,28 @@
+function renderTodayDate() {
+  const experienceStartedOnYear = 2009;
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split('T')[0];
+  const elements = document.querySelectorAll('.js-today-date');
+  elements.forEach(element => {
+    element.datetime = formattedDate;
+    // element.textContent = formattedDate;
+  });
+  document.querySelector('.js-experience-years').textContent = currentDate.getFullYear() - experienceStartedOnYear;
+}
+
+function showActiveMenuLink() {
+  const activeClassName = 'sidebar__navigation-menu-link--active';
+  const links = document.querySelectorAll('.js-sidebar-navigation-menu-link');
+
+  links.forEach(link => {
+    link.classList.remove(activeClassName);
+    if (link.href === window.location.href) {
+      link.classList.add(activeClassName);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderTodayDate();
+  showActiveMenuLink();
+});
